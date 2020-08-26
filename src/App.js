92450ab6b845ab2d6ffdd9859211e4d00f586645
waleedomega <waleedomega@hotmail.com>
2020-08-26 11:40:28 +0200

    add libraries

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to the Swag Shop</h1>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
